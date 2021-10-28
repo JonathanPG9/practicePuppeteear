9faaf7582a949fe4faf1a1b930b32604d62b6f45
Jonathan Paniagua <jonatanpaniagua21@gmail.com>
2021-10-28 03:28:36 -0300

    uploading to github

---
+++ index.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+( async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.amazon.com/',{
+      waitUntil: 'networkidle2',
+    });
+    await page.screenshot({path: 'test1.jpg'});
+    const html = await page.content()
+    fs.writeFile('test.html',html,(err) => {
+      if(err) return err
+      console.log('html saved')
+    })
+    await browser.close();
+  }
+  catch(err) {
+    return err
+  }
+})();
