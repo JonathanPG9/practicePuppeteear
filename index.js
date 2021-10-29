033d97fceb8974bff2f9e41931e8d3a4023c344e
Jonathan Paniagua <jonatanpaniagua21@gmail.com>
2021-10-29 00:49:51 -0300

    update

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const data = require('./urls.json');
+const path = require('path');
+const dist = path.join(__dirname,"/","dist");
 
 ( async () => {
   try {
-    const browser = await puppeteer.launch({
-      defaultViewport: {width: 1920, height: 1080}
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.amazon.com/',{
-      waitUntil: 'networkidle2',
-    });
-    await page.screenshot({path: 'test.jpg'});
-    const html = await page.content()
-    fs.writeFile('test.html',html,(err) => {
-      if(err) return err
-      console.log('html saved')
-    })
-    await browser.close();
+    const dataLength = data.urls.length;
+    for (let i = 0; i < dataLength; i++) {
+      await page.goto(data.urls[i].url,{
+          waitUntil: 'networkidle2',
+        });
+      let html = await page.content()
+      fs.writeFile(`${data.urls[i].name}.html`,html,(err) => {
+        if(err) throw err;
+        console.log(`${data.urls[i].name}.html created`)
+        fs.rename(__dirname + `/${data.urls[i].name}.html`,dist + `/${data.urls[i].name}.html`, (err) => {
+          if (err) throw err;
+          console.log(`Move ${data.urls[i].name}.html complete.`);
+        });
+      })
+      if(i === data.urls.length - 1) return await browser.close();
+    }
   }
   catch(err) {
     return err
