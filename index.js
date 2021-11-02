c2e92cfa39eb3495901a3234531189f2157a6ec2
Jonathan Paniagua <jonatanpaniagua21@gmail.com>
2021-11-02 15:46:17 -0300

    update

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const puppeteer = require('puppeteer');
 const fs = require('fs');
 const data = require('./urls.json');
 const path = require('path');
-const dist = path.join(__dirname,'/','dist');
-const dataLength = data.urls.length;
+const dist = path.join(__dirname,'/dist');
 const exec = require('child_process').exec;
+const urls = data.urls;
+const dataLength = data.urls.length;
 
-(() => { 
+(() => {
   for (let i = 0; i < dataLength; i++) {
-    let command = `curl ${data.urls[i].url}`
-    exec(command, (err, stdout, stderr) => {
-      if (err) {
-        return err;
-      }
-      console.log(`stdout: ${stdout}`);
-    fs.writeFile(`${data.urls[i].name}.html`,stdout,(err) => {
+    const command = `curl ${urls[i]?.url}`;
+    exec(command, (err, stdout) => {
       if(err) throw err;
-      console.log(`${data.urls[i].name}.html created`);
-      fs.rename(__dirname + `/${data.urls[i].name}.html`,dist + `/${data.urls[i].name}.html`, (err) => {
-        if (err) throw err;
-        console.log(`Move ${data.urls[i].name}.html complete.`);
+      console.log(`stdout: ${stdout}`);
+    fs.writeFile(`${urls[i].name}.html`, stdout, (err) => {
+      if(err) throw err;
+      console.log(`${urls[i].name}.html created`);
+      fs.rename(`${__dirname}/${urls[i].name}.html`,`${dist}/${urls[i].name}.html`, (err) => {
+        if(err) throw err;
+        console.log(`Move ${urls[i].name}.html complete.`);
+        });
       });
-    });
-  }); 
-  }
+    }); 
+  };
 })();
