dfad30ddfff0ff57be20cbb41d18024477d57518
Jonathan Paniagua <jonatanpaniagua21@gmail.com>
2021-11-02 16:27:32 -0300

    update

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const fs = require('fs'),
     const command = `curl ${urls[i]?.url}`,
           name = urls[i].name;
     exec(command, (err, stdout) => {
-      if(err) throw err;
+      if (err) throw err;
       console.log(`stdout: ${stdout}`);
-    fs.writeFile(`${dist}/${name}.html`, stdout, (err) => {
-      if(err) throw err;
-      console.log(`${name}.html created`);
+      fs.writeFile(`${dist}/${name}.html`, stdout, (err) => {
+        if (err) throw err;
+        console.log(`${name}.html created`);
       });
-    }); 
-  };
+    });
+  }
 })();
