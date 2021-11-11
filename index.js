1e4ff1d89c160805d2792c945e53f474ddf0d166
Jonathan Paniagua <jonatanpaniagua21@gmail.com>
2021-11-11 11:03:17 -0300

    update

--- index.js
+++ index.js
@@ -5,42 +5,40 @@ const fs = require('fs'),
       dist = path.join(__dirname,'/dist'),
       urls = data.urls,
       dataLength = data.urls.length,
-      classNames = ["mapbox","usat-interactive-graphic"]
+      classNames = ["mapbox","usat-interactive-graphic"],
       classNamesLength = classNames.length;
 // let count = 0; // the purpose of the count variable is just to test the same url with mapbox because writeFile overwrites the file with the same name
 (() => {
   for (let i = 0; i < dataLength; i++) {
     const command = `curl ${urls[i]?.url}`,
           name = urls[i].name,
-          domainName = `${urls[i]?.url.split("/")[2]}`,
+          domainName = `${urls[i]?.url.split('/')[2]}`,
           folderName = `${dist}/${domainName}`;
-    if (!fs.existsSync(folderName)){
-        fs.mkdirSync(folderName);
-    };
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName);
+    }
     exec(command, (err, stdout) => {
       if (err) throw err;
-      let index = 0
-      while(classNamesLength > index) {
-        if(stdout.includes(classNames[index])) {
-          if (!fs.existsSync(`${folderName}/mapbox`)){
+      let index = 0;
+      while (classNamesLength > index) {
+        if (stdout.includes(classNames[index])) {
+          if (!fs.existsSync(`${folderName}/mapbox`)) {
             fs.mkdirSync(`${folderName}/mapbox`);
-          };
-          if(fs.existsSync(`${folderName}/${name}.html`)) {
-            fs.renameSync(`${folderName}/${name}.html`,`${folderName}/mapbox/${name}.html`)
           }
-          else {
+          if (fs.existsSync(`${folderName}/${name}.html`)) {
+            fs.renameSync(`${folderName}/${name}.html`, `${folderName}/mapbox/${name}.html`);
+          } else {
             fs.writeFile(`${folderName}/mapbox/${name}.html`, stdout, (err) => {
               if (err) throw err;
             });
           }
-        }
-        else {
+        } else {
           fs.writeFile(`${folderName}/${name}.html`, stdout, (err) => {
-          if (err) throw err;
-        });
-        };
-        index++
-      };
+            if (err) throw err;
+          });
+        }
+        index++;
+      }
     });
   }
 })();
