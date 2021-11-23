0fce8982d6742690a939e579c0dc0f32e5cb96b5
Jonathan Paniagua <jonatanpaniagua21@gmail.com>
2021-11-23 19:02:03 -0300

    Refactoring IIFE

--- index.js
+++ index.js
@@ -9,42 +9,27 @@ const fs = require('fs'),
 (() => {
   for (let i = 1; i < dataLength; i++) {
     const command = `curl ${urls[i]}`,
-          domainName = `${urls[i]?.split('.')[1]}`,
-          pathUrl = `${urls[i]?.split('/')[3]}`,
-          folderName = `${static}/${domainName}`,
-          pathFolder = `${folderName}/${pathUrl}`;
-    if (!fs.existsSync(folderName)) {
-      fs.mkdir(folderName,(err) => {
-        if(err) throw err
-      })
-    }
-    if (!fs.existsSync(pathFolder)) {
-      fs.mkdir(pathFolder,(err) => {
-        if(err) throw err
-      })
-    }
-    exec(command, (err, stdout) => {
-      if (err) console.log(err)
-      let index = 0;
-      while (classNamesLength > index) {
-        if (stdout.includes(classNames[index])) {
-          if (!fs.existsSync(`${pathFolder}/${classNames[index]}`)) {
-            fs.mkdirSync(`${pathFolder}/${classNames[index]}`);
-          }
-          if (fs.existsSync(`${pathFolder}/index.html`)) {
-            fs.renameSync(`${pathFolder}/index.html`, `${pathFolder}/${classNames[index]}/index.html`);
-          } else {
-            fs.writeFile(`${pathFolder}/${classNames[index]}/index.html`, stdout, (err) => {
-              if (err) throw err;
+      domainName = `${urls[i]?.split('.')[1]}`,
+      pathUrl = `${urls[i]?.split('/')[3]}`,
+      folderName = `${static}/${domainName}`,
+      pathFolder = `${folderName}/${pathUrl}`;
+    fs.stat(folderName, function (err) {
+      if (err.code === 'ENOENT') {
+        // file does not exist
+        fs.mkdir(folderName, () => {
+          fs.mkdir(pathFolder, (err) => {
+            if (err) return err
+            exec(command, (err, stdout) => {
+              if (err) console.log(err)
+              fs.writeFile(`${pathFolder}/index.html`, stdout, (err) => {
+                if (err) console.log(err)
+              });
             });
-          }
-        } else {
-          fs.writeFile(`${pathFolder}/index.html`, stdout, (err) => {
-            if (err) throw err;
-          });
-        }
-        index++;
+          })
+        })
+      } else {
+        console.log(err);
       }
-    });
+    })
   }
 })();
