7bd97378918f1e22adbf48774090ee9c84df83b8
Jonathan <jonathan.paniagua@globant.com>
2021-11-25 17:48:32 -0300

    refactoring mkdir with recursion

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
 const fs = require('fs'),
       path = require('path'),
       exec = require('child_process').exec,
-      static = path.join(__dirname,'/static'),
+      destinationFolder = path.join(__dirname,'/static'),
       {urls} = require("./urlsString");
       dataLength = urls.length,
       classNames = ["mapbox","usat-interactive-graphic"],
       classNamesLength = classNames.length;
+let message = "";
 (() => {
   for (let i = 1; i < dataLength; i++) {
     const command = `curl ${urls[i]}`,
-      domainName = `${urls[i]?.split('.')[1]}`,
-      pathUrl = `${urls[i]?.split('/')[3]}`,
-      folderName = `${static}/${domainName}`,
-      pathFolder = `${folderName}/${pathUrl}`;
-    fs.stat(folderName, (err) => {
-      if (err.code === null) return console.log(`${folderName} exist`); // folder exist
-      if (err.code === 'ENOENT') {
-        // folder does not exist
-        fs.mkdir(folderName, () => {
-          fs.mkdir(pathFolder, (err) => {
-            if (err) console.log(err);
-            exec(command, (err, stdout, stderr) => {
-              if (err) {
-                console.log(err)
-                const message = `${command}\n ${stderr}`;
-                fs.appendFile(`error.txt`, message, (error) => {
-                  if (error) console.log(error);
-                });
-              }
-              fs.writeFile(`${pathFolder}/index.html`, stdout, (err) => {
-                if (err) console.log(err);
-              });
-            });
-          });
+          domainName = `${urls[i]?.split('.')[1]}`,
+          pathUrl = `${urls[i].slice(urls[i].indexOf(".com/") + 5, urls[i].length - 2)}`,
+          pathFolder = `${destinationFolder}/${domainName}/${pathUrl}`;
+    exec(command, (err, stdout) => {
+      if (err) {
+        console.log(err)
+        message += command.slice(5);
+        fs.appendFile(`${destinationFolder}/error.txt`, message, (err) => {
+          if (err) console.log(err);
+        })
+      }
+      fs.mkdir(pathFolder, { recursive: true }, (err) => {
+        if (err) console.log(err);
+        fs.writeFile(`${pathFolder}/index.html`, stdout, (err) => {
+          if (err) console.log(err);
         });
-      };
-    });
+      })
+    })
   }
 })();
