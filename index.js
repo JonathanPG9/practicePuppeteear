7c0751ce912b78dd0698aacdedb3b91dd0674917
Jonathan <jonathan.paniagua@globant.com>
2022-03-02 05:17:49 -0300

    Curl -H with host

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 const fs = require('fs'),
       path = require('path'),
       exec = require('child_process').exec,
-      destinationFolder = path.join(__dirname,'/interactives'),
+      destinationFolder = path.join(__dirname,'/newTask'),
       urls = require("./urls"),
       dataLength = urls.length;
 let messageError = '';
 (() => {
   fs.mkdirSync(destinationFolder);
   for (let i = 1; i < dataLength; i++) {
-    const command = `curl ${urls[i]}`,
-          domainName = `${urls[i]?.split('.')[1]}`,
+    const domainName = `${urls[i]?.split('.')[1]}`,
           pathUrl = `${urls[i].slice(urls[i].indexOf(".com/") + 5, urls[i].trim().length - 1)}`,
-          pathFolder = `${destinationFolder}/${domainName}/${pathUrl}`;
+          pathFolder = `${destinationFolder}/${domainName}/${pathUrl}`,
+          command = `curl -H Host:static.${domainName}.com https://usat-network-static.herokuapp.com/${pathUrl}/`;
+
     exec(command, (err, stdout) => {
-      if (err) {
-        console.log(err)
-        messageError += command.slice(5);
-        fs.appendFile(`${destinationFolder}/error.txt`, messageError, (err) => {
-          if (err) console.log(err);
-        })
-      }
-      else {
         fs.mkdir(pathFolder, { recursive: true }, (err) => {
           if (err) console.log(err);
           fs.writeFile(`${pathFolder}/index.html`, stdout, (err) => {
             if (err) console.log(err);
           });
         })
-      }
     })
   }
 })();
